3d2ee6c4a22bc92c37a7fd66249fcff80c190b49
Drew Bradford <16455249+DrewBradfordXYZ@users.noreply.github.com>
2025-02-19 23:45:39 -0500

    remove ./dist from gitignore

---
+++ dist/codePagesUpdate.js
@@ -0,0 +1,217 @@
+#!/usr/bin/env node
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import puppeteer from "puppeteer";
+import * as fs from "fs";
+import * as path from "path";
+import * as dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import chalk from "chalk";
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url); // this line is needed to get the absolute path of the current file in an ES6 module,
+const __dirname = path.dirname(__filename);
+const getAllFiles = (extension, directory = "assets") => {
+    const assetsDir = path.resolve(process.cwd(), `./dist/${directory}`);
+    let files;
+    try {
+        files = fs.readdirSync(assetsDir);
+    }
+    catch (error) {
+        throw new Error(`Error reading directory ${assetsDir}: ${error.message}`);
+    }
+    const filteredFiles = files.filter((file) => file.endsWith(extension));
+    if (filteredFiles.length === 0) {
+        throw new Error(`No files found in dist/${directory} directory with extension ${extension}`);
+    }
+    return filteredFiles.map((file) => path.join(assetsDir, file));
+};
+const generateScreenshotPath = (filename) => {
+    const libraryRoot = path.resolve(__dirname, ".."); // Go up one level from dist
+    return path.join(libraryRoot, "screenshots-puppeteer", filename);
+};
+const captureScreenshot = (page, filename) => __awaiter(void 0, void 0, void 0, function* () {
+    const screenshotPath = generateScreenshotPath(filename);
+    try {
+        yield page.screenshot({ path: screenshotPath });
+        console.log(`Screenshot saved to ${screenshotPath}`);
+    }
+    catch (error) {
+        console.error(`Error taking screenshot: ${error.message}`);
+    }
+});
+const logNavigationAttempt = (attempt, url) => {
+    const message = attempt === 1
+        ? `${chalk.bold.whiteBright("Navigating to")} ${chalk.blue(url)}`
+        : chalk.bold.whiteBright(`Attempt ${attempt}: Navigating to ${chalk.blue(url)}`);
+    console.log(message);
+};
+const extractPageName = (page) => __awaiter(void 0, void 0, void 0, function* () {
+    return yield page.evaluate(() => {
+        const nameInput = document.querySelector('input[name="name"]');
+        return nameInput ? nameInput.value : "Unknown";
+    });
+});
+const updateCodePages = () => __awaiter(void 0, void 0, void 0, function* () {
+    const quickbaseUrl = process.env.QUICKBASE_LOGIN_URL;
+    const quickbasePagePath = process.env.QUICKBASE_CODEPAGE_EDIT_URL;
+    const username = process.env.QUICKBASE_USERNAME;
+    const password = process.env.QUICKBASE_PASSWORD;
+    const htmlPageId = process.env.QUICKBASE_CODEPAGE_HTML_ID;
+    const jsPageIds = process.env.QUICKBASE_CODEPAGE_JS_IDS.split(",");
+    const cssPageIds = process.env.QUICKBASE_CODEPAGE_CSS_IDS.split(",");
+    const jsFiles = getAllFiles(".js");
+    const cssFiles = getAllFiles(".css");
+    const htmlFiles = getAllFiles(".html", ""); // Specify the root dist directory for HTML files
+    // --no-sandbox is required when running Puppeteer on a Linux server
+    const browser = yield puppeteer.launch({
+        headless: true,
+        args: ["--no-sandbox"],
+    });
+    const page = yield browser.newPage();
+    try {
+        // Log in to QuickBase
+        console.log(chalk.bold.underline.whiteBright("Logging in to QuickBase"));
+        let loginSuccess = false;
+        const maxLoginAttempts = 3;
+        let loginAttempt = 0;
+        while (!loginSuccess && loginAttempt < maxLoginAttempts) {
+            try {
+                loginAttempt++;
+                if (loginAttempt > 1) {
+                    console.log(chalk.blue(`Login attempt ${loginAttempt}`));
+                }
+                yield page.goto(quickbaseUrl, { timeout: 60000 });
+                yield page.waitForSelector("input[name='loginid']", { timeout: 60000 });
+                yield page.type("input[name='loginid']", username);
+                yield page.type("input[name='password']", password);
+                yield page.click("#signin");
+                yield page.waitForNavigation({
+                    waitUntil: "networkidle0",
+                    timeout: 60000,
+                });
+                // Check if login was successful
+                const loginError = yield page.$(".login-error");
+                if (loginError) {
+                    throw new Error("Login failed. Please check your credentials.");
+                }
+                console.log(chalk.bold.green("Signed In to QuickBase."));
+                loginSuccess = true;
+            }
+            catch (loginError) {
+                console.error(chalk.red(`Login attempt ${loginAttempt} failed: ${loginError.message}`));
+                if (loginAttempt >= maxLoginAttempts) {
+                    console.error(chalk.bold.bgRed("Max login attempts reached. Exiting."));
+                    yield browser.close();
+                    return;
+                }
+                // Add a delay before retrying
+                yield new Promise((resolve) => setTimeout(resolve, 5000));
+            }
+        }
+        // Function to update code page content
+        const updatePageContent = (pageId, codeContent, filePath) => __awaiter(void 0, void 0, void 0, function* () {
+            const url = `${quickbasePagePath}${pageId}`;
+            const maxRetries = 3;
+            let attempt = 0;
+            let success = false;
+            let codePageName = "Unknown"; // Initialize pageName
+            while (attempt < maxRetries && !success) {
+                try {
+                    attempt++;
+                    logNavigationAttempt(attempt, url);
+                    yield page.goto(url, { timeout: 30000 }); // 30 seconds timeout
+                    yield page.waitForSelector("#pagetext", { timeout: 30000 }); // Wait for the element where the code goes
+                    // Extract the value of the name field
+                    codePageName = yield extractPageName(page);
+                    console.log(`${chalk.bold.whiteBright(`Opened code-page-${pageId}`)}`);
+                    success = true;
+                }
+                catch (error) {
+                    console.error(chalk.yellow(`Attempt ${attempt}: Failed to navigate to code-page-${pageId}`));
+                    const enableErrorDetails = false; // Set to true to display error details
+                    if (attempt >= maxRetries) {
+                        console.error(chalk.bold.bgRed("Max retries reached."));
+                        console.error(chalk.bold.red(`${url}`));
+                        if (enableErrorDetails) {
+                            console.error(error);
+                        }
+                        return;
+                    }
+                    // Add a delay before retrying
+                    yield new Promise((resolve) => setTimeout(resolve, 5000));
+                }
+            }
+            // Update the code page content
+            yield page.evaluate((codeContent) => {
+                const pageText = document.querySelector("#pagetext");
+                if (pageText) {
+                    pageText.value = codeContent;
+                }
+                else {
+                    throw new Error("Code editor element not found");
+                }
+            }, codeContent);
+            // Save the changes
+            yield page.click("#btnSaveDone");
+            console.log(`${chalk.bold.whiteBright("Updating")} ${chalk.hex("#FFA500")(`${codePageName}`)} ${chalk.bold.whiteBright("with")} ${chalk.hex("#FFA500")(path.basename(filePath))}`);
+            yield page.waitForNavigation();
+            console.log(chalk.bold.bgGreen(`Successfully Saved`));
+        });
+        // Update HTML code page if htmlPageId is not empty
+        if (htmlPageId && htmlFiles.length > 0) {
+            const htmlFilePath = htmlFiles[0];
+            const htmlCodeContent = fs.readFileSync(htmlFilePath, "utf8");
+            yield updatePageContent(htmlPageId, htmlCodeContent, htmlFilePath);
+        }
+        // Update JavaScript code pages
+        for (let i = 0; i < jsFiles.length; i++) {
+            const jsFilePath = jsFiles[i];
+            const jsCodeContent = fs.readFileSync(jsFilePath, "utf8");
+            const jsPageId = jsPageIds[i];
+            yield updatePageContent(jsPageId, jsCodeContent, jsFilePath);
+        }
+        // Update CSS code pages
+        for (let i = 0; i < cssFiles.length; i++) {
+            const cssFilePath = cssFiles[i];
+            const cssCodeContent = fs.readFileSync(cssFilePath, "utf8");
+            const cssPageId = cssPageIds[i];
+            yield updatePageContent(cssPageId, cssCodeContent, cssFilePath);
+        }
+        // Set to true to test screenshot capture
+        if (false) {
+            throw new Error("Intentional error for testing screenshot capture.");
+        }
+        // Delete the screenshot file after successful execution
+        const screenshotPath = generateScreenshotPath("error_codepage_screenshot.png");
+        if (fs.existsSync(screenshotPath)) {
+            fs.unlinkSync(screenshotPath);
+            console.log("Script ran without errors, screenshot error file deleted successfully.");
+        }
+    }
+    catch (error) {
+        console.error("Something went wrong, check the screenshot", error);
+        try {
+            // Take a screenshot for debugging
+            yield captureScreenshot(page, "error_codepage_screenshot.png");
+        }
+        catch (screenshotError) {
+            console.error("Error capturing screenshot:", screenshotError);
+        }
+    }
+    finally {
+        yield browser.close();
+    }
+});
+// Export the function for external use
+export { updateCodePages };
+// If the script is run directly, call the function
+if (import.meta.url === `file://${__filename}`) {
+    updateCodePages();
+}
---
+++ dist/createHtmlCodePage.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+import fs from "fs";
+import path from "path";
+import { findUp } from "find-up";
+import dotenv from "dotenv";
+dotenv.config();
+function generateHtml() {
+    const htmlTitle = process.env.QUICKBASE_HTML_PAGE_TITLE || "QuickBase";
+    const cssPageIds = process.env.QUICKBASE_CODEPAGE_CSS_IDS
+        ? process.env.QUICKBASE_CODEPAGE_CSS_IDS.split(",")
+        : ["<-QUICKBASE-CODEPAGE-CSS-IDS->"];
+    const jsPageIds = process.env.QUICKBASE_CODEPAGE_JS_IDS
+        ? process.env.QUICKBASE_CODEPAGE_JS_IDS.split(",")
+        : ["<-QUICKBASE-CODEPAGE-JS-IDS->"];
+    const quickbasePagesUrl = process.env.QUICKBASE_CODEPAGES_URL || "<-QUICKBASE-CODEPAGES-URL->";
+    const cssLinks = cssPageIds
+        .map((id, index) => `${index === 0 ? "" : "  "}\t<link href="${quickbasePagesUrl}?a=dbpage&pageID=${id.trim()}" rel="stylesheet">`)
+        .join("\n");
+    const jsScripts = jsPageIds
+        .map((id, index) => `${index === 0 ? "" : "  "}\t<script src="${quickbasePagesUrl}?a=dbpage&pageID=${id.trim()}"></script>`)
+        .join("\n");
+    const commentsPageUrl = process.env.QUICKBASE_CODEPAGES_URL
+        ? ""
+        : "    <!-- Update QUICKBASE_CODEPAGES_URL in the .env file. -->";
+    const commentsCssPageIds = process.env.QUICKBASE_CODEPAGE_CSS_IDS
+        ? ""
+        : "    <!-- Update QUICKBASE_CODEPAGE_CSS_IDS in the .env file. -->";
+    const commentsJsPageIds = process.env.QUICKBASE_CODEPAGE_JS_IDS
+        ? ""
+        : "    <!-- Update QUICKBASE_CODEPAGE_JS_IDS in the .env file. -->";
+    let commentsEnvRename = "";
+    if (commentsPageUrl && commentsCssPageIds && commentsJsPageIds) {
+        commentsEnvRename =
+            "    <!-- Make sure to rename the .env.example file to .env -->";
+    }
+    let htmlContent = `
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>${htmlTitle}</title>`;
+    if (commentsEnvRename)
+        htmlContent += `\n${commentsEnvRename}`;
+    if (commentsPageUrl)
+        htmlContent += `\n${commentsPageUrl}`;
+    if (commentsCssPageIds)
+        htmlContent += `\n${commentsCssPageIds}`;
+    htmlContent += `
+  ${cssLinks}
+</head>
+<body>
+  <noscript>
+    <strong>
+    We're sorry but this app doesn't work properly without
+    JavaScript enabled. Please enable it to continue.
+    </strong>
+  </noscript>
+  <div id="root"></div>`;
+    if (commentsPageUrl)
+        htmlContent += `\n${commentsPageUrl}`;
+    if (commentsJsPageIds)
+        htmlContent += `\n${commentsJsPageIds}`;
+    htmlContent += `
+  ${jsScripts}
+</body>
+</html>`;
+    // Get the root project name
+    findUp(".git", { type: "directory" }).then((gitRootPath) => {
+        const rootFolderName = gitRootPath
+            ? path.basename(path.dirname(gitRootPath))
+            : "codepage"; // if git is not found, use codepage as the default name
+        // Writing to file
+        fs.writeFileSync(path.join(process.cwd(), `./dist/${rootFolderName}.html`), htmlContent, "utf8");
+    });
+}
+generateHtml();
+// If the script is run directly, call the function
+if (import.meta.url === `file://${process.argv[1]}`) {
+    generateHtml();
+}
